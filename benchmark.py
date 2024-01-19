c399db84f4b1f7432534887acb80ad1d16a7a8dd
Robert Shaw <114415538+rib-2@users.noreply.github.com>
2024-01-19 15:04:27 -0500

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -3,6 +3,14 @@ import time
 import argparse
 from vllm import LLM, SamplingParams
 
+model_id = "meta-llama/Llama-2-7b-chat-hf"
+DO_DECODE = True
+DO_PREFILL = True
+DECODE_BATCH_SIZES = [1,2,4,8,16,32,64]
+PREFILL_BATCH_SIZES = [1,2,4,8]
+PREFILL_WORD_LENS = [16, 128, 256, 512, 1024]
+CUT_PROMPT = True
+
 def get_decode_tput(batch_size=1, num_tokens=256):
     prompts = ["Hello"] * batch_size
     sampling_params = SamplingParams(max_tokens=num_tokens, ignore_eos=True)
@@ -22,8 +30,12 @@ def get_decode_tput(batch_size=1, num_tokens=256):
     print(f"Total tokens: {total_tokens} tokens")
     print(f"Tput: {total_tokens / total_time:0.2f} tokens/sec")
 
-def get_prefill_tput(num_words=32, iterations=10):
-    prompts = ("Hello_" * num_words)[:-1]
+def get_prefill_tput(batch_size=1, num_words=32, iterations=10):
+    if CUT_PROMPT:
+        prompts = [("Hello_" * num_words)[:-1]]*batch_size
+    else:
+        prompts = ["Hello_" * num_words]*batch_size
+
     sampling_params = SamplingParams(max_tokens=1)
 
     start = time.perf_counter()
@@ -31,25 +43,30 @@ def get_prefill_tput(num_words=32, iterations=10):
         outputs = model.generate(prompts, sampling_params)
     torch.cuda.synchronize()
     end = time.perf_counter()
-
+    
     total_time = end - start
-    prompt_tokens = len(outputs[0].prompt_token_ids)
+    
+    prompt_tokens = 0
+    for output in outputs:
+        prompt_tokens += len(output.prompt_token_ids)
     total_tokens = prompt_tokens * iterations
-
-    print(f"\n----- Prompt Tokens: {prompt_tokens} -----")
+    
+    print(f"\n----- Batch Size: {batch_size} -----")
+    print(f"----- Prompt Tokens: {prompt_tokens // batch_size} -----")
     print(f"Total time: {total_time:0.2f}s")
     print(f"Total tokens: {total_tokens} tokens")
     print(f"Tput: {total_tokens / total_time:0.2f} tokens/sec\n")
 
 if __name__ == "__main__":
-    model = LLM(model="meta-llama/Llama-2-7b-chat-hf")
-
-    # benchmark decode
-    batch_sizes = [1,2,4,8,16,32,64]
-    for batch_size in batch_sizes:
-        get_decode_tput(batch_size, num_tokens=256)
-
-    # benchmark prefill
-    num_words_list = [16, 64, 128, 256, 512, 1024, 2048]
-    for num_words in num_words_list:
-        get_prefill_tput(num_words, iterations=10)
+    model = LLM(model=model_id)
+    
+    if DO_DECODE:
+        # benchmark decode
+        for batch_size in DECODE_BATCH_SIZES:
+            get_decode_tput(batch_size, num_tokens=256)
+    
+    if DO_PREFILL:
+        # benchmark prefill
+        for batch_size in PREFILL_BATCH_SIZES:
+            for num_words in PREFILL_WORD_LENS:
+                get_prefill_tput(batch_size, num_words, iterations=10)
