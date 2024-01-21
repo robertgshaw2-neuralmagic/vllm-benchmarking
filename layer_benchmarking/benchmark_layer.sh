1b9633eb2268be441341ed3e254fbfebcd293462
rib-2 <rshaw@neuralmagic.com>
2024-01-21 18:19:05 +0000

    added a10 layer benchmarks

--- layer_benchmarking/benchmark_layer.sh
+++ layer_benchmarking/benchmark_layer.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 do_10s=false
-gpu_type="a100"
+gpu_type="a10"
 model_sizes=("7b-mha" "7b-gqa" "13b-mha" "13b-gqa" "33b-gqa" "70b-gqa")
-layer_types=("qkv_linear")
+layer_types=("column_linear")
 
 for model_size in ${model_sizes[@]}; do
     for layer_type in ${layer_types[@]}; do
-        echo $"------ Starting $model_size $layer_type \n"
+        echo "------ Starting $model_size $layer_type"
+	echo ""
 
         if (( $do_10s )); then
             python3 benchmark_layer.py --model-size $model_size --layer-type $layer_type --gpu-type $gpu_type --skip-gptq --skip-awq --use-10s-input
