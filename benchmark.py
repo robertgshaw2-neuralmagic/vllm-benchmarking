c2b5b7e5c348c8fe494a95d5918244ff978b85c2
rib-2 <rshaw@neuralmagic.com>
2024-01-21 21:17:49 +0000

    stash

--- benchmark.py
+++ benchmark.py
@@ -9,39 +9,54 @@ parser.add_argument("--skip-decode", action="store_true")
 parser.add_argument("--skip-prefill", action="store_true")
 parser.add_argument("--skip-cut-prompt", action="store_true")
 
-DECODE_BATCH_SIZES = [1,2,4,8,16,32,64]
-PREFILL_BATCH_SIZES = [1,2,4,8]
-PREFILL_WORD_LENS = [16, 128, 256, 512]
+GPU_TYPES = ["a100", "a10", "a4000"]
 
-def get_decode_tput(batch_size=1, num_tokens=256):
+DECODE_BATCH_SIZES = [1, 4, 8, 16, 32, 48, 64]
+PREFILL_BATCH_SIZES = [1]
+PREFILL_WORD_LENS = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+
+def get_decode_tput(batch_size=1, num_tokens=256, iterations=10, _verbose=False):
     prompts = ["Hello"] * batch_size
     sampling_params = SamplingParams(max_tokens=num_tokens, ignore_eos=True)
 
-    start = time.perf_counter()
+    print("warming up...")
     outputs = model.generate(prompts, sampling_params)
+    torch.cuda.synchronize()
+
+    print("benchmarking...")
+    start = time.perf_counter()
+    for _ in range(iterations):
+        outputs = model.generate(prompts, sampling_params)
     torch.cuda.synchronize()
     end = time.perf_counter()
 
-    total_time = end - start
     total_tokens = 0
     for output in outputs:
         total_tokens += len(output.outputs[0].token_ids)
-
+    
+    total_tokens *= iterations
     tput = total_tokens / total_time
-    print(f"----- Batch Size: {batch_size} -----")
-    print(f"Total time: {total_time:0.2f}s")
-    print(f"Total tokens: {total_tokens} tokens")
-    print(f"Tput: {tput:0.2f} tokens/sec\n")
+    
+    if _verbose:
+        print(f"----- Batch Size: {batch_size} -----")
+        print(f"Total time: {total_time:0.2f}s")
+        print(f"Total tokens: {total_tokens} tokens")
+        print(f"Tput: {tput:0.2f} tokens/sec\n")
 
     return tput
 
-def get_prefill_tput(batch_size=1, num_words=32, iterations=10, cut_prompt=True):
+def get_prefill_tput(batch_size=1, num_words=32, iterations=100, cut_prompt=True, _verbose=False):
     if cut_prompt:
         prompts = [("Hello_" * num_words)[:-1]]*batch_size
     else:
         prompts = ["Hello_" * num_words]*batch_size
 
     sampling_params = SamplingParams(max_tokens=1)
+    
+    warmup_iterations = 3
+    for _ in range(warmup_iterations):
+        outputs = model.generate(prompts, sampling_params)
+    torch.cuda.synchronize()
 
     start = time.perf_counter()
     for _ in range(iterations):
@@ -57,11 +72,13 @@ def get_prefill_tput(batch_size=1, num_words=32, iterations=10, cut_prompt=True)
     total_tokens = prompt_tokens * iterations
     
     tput = total_tokens / total_time
-    print(f"\n----- Batch Size: {batch_size} -----")
-    print(f"----- Prompt Tokens: {prompt_tokens // batch_size} -----")
-    print(f"Total time: {total_time:0.2f}s")
-    print(f"Total tokens: {total_tokens} tokens")
-    print(f"Tput: {tput:0.2f} tokens/sec\n")
+
+    if _verbose:
+        print(f"\n----- Batch Size: {batch_size} -----")
+        print(f"----- Prompt Tokens: {prompt_tokens // batch_size} -----")
+        print(f"Total time: {total_time:0.2f}s")
+        print(f"Total tokens: {total_tokens} tokens")
+        print(f"Tput: {tput:0.2f} tokens/sec\n")
 
     return tput
 
@@ -69,20 +86,28 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     model = LLM(model=args.model_id)
-    results = {}
+    results = []
 
     if not args.skip_decode:
         # benchmark decode
         for batch_size in DECODE_BATCH_SIZES:
             tput = get_decode_tput(batch_size, num_tokens=256)
-            results[f"decode_b_{batch_size}"] = tput
+            results.append({
+                "model_id": args.model_id,
+                "seq_len": 1,
+                "batch_size": batch,
+            })
 
     if not args.skip_prefill:
         # benchmark prefill
         for batch_size in PREFILL_BATCH_SIZES:
             for num_words in PREFILL_WORD_LENS:
                 tput = get_prefill_tput(batch_size, num_words, iterations=10, cut_prompt=(not args.skip_cut_prompt))
-                results[f"prefill_b_{batch_size}_seqlen_{num_words*2}"] = tput
+                results.append({
+                    "model_id": args.model_id,
+                    "seq_len": 1,
+                    "batch_size": batch,
+                })
 
     for key in results:
         print(f"{key}: {results[key]: 0.0f} tok/sec")
